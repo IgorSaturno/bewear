58653a0ad0510bcb440700e4dd3e4ff161d093b2
igor nascimento saturnino <igornascimentosaturnino11@gmail.com>
2025-08-06 20:57:42 -0300

    feat: add sign out

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -90,7 +90,7 @@ const SignUpForm = () => {
 
   return (
     <>
-      <Card>
+      <Card className="w-full">
         <CardHeader>
           <CardTitle>Criar conta</CardTitle>
           <CardDescription>Crie uma conta para continuar.</CardDescription>
